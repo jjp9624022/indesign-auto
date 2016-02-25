fd0b79142d5e5257f238ad3dbbba06c7d81809c0
leweng <jjp9624022@gmail.com>
2016-02-25 11:24:45 +0800

    new file:   changeMainPage.jsx

---
+++ changeMainPage.jsx
@@ -0,0 +1,22 @@
+﻿main();
+function main(){
+
+var myText=app.findGrep();
+for  (var i=0; i<myText.length;i++)
+{
+    myText[i].parentTextFrames[0].parentPage.appliedMaster=NothingEnum.NOTHING;
+    myText[i].parentTextFrames[0].nextTextFrame.parentPage.appliedMaster=NothingEnum.NOTHING;
+//跨页的操作       
+       // myText[i].parentTextFrames[0].nextTextFrame.parentPage.parent.appliedMaster=NothingEnum.NOTHING;
+
+    
+    }
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+app.findChangeGrepOptions.includeFootnotes = false;
+app.findChangeGrepOptions.includeHiddenLayers = false;
+app.findChangeGrepOptions.includeLockedLayersForFind = false;
+app.findChangeGrepOptions.includeLockedStoriesForFind = false;
+app.findChangeGrepOptions.includeMasterPages = false;
+}
+
